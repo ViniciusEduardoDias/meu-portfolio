43d3f506a90f439369a9c129471d5f552f9eb973
Vinícius Dias <diasvinicius0401@gmail.com>
2025-04-15 19:33:54 -0300

    finish

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
 import ListItem from "./ListItem";
-//import Image from "next/image";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="sm:fixed sm:top-0 sm:left-0 w-full z-50 bg-black text-gray-50 flex flex-col sm:flex-row space-y-4 justify-evenly p-6 items-center shadow-neon">
-      <div id="logo">
-        <h1 className="text-3xl font-bold text-white text-glow animate-textGlow">
-          VD
-        </h1>
+      <div
+        id="logo"
+        className="flex items-center gap-3 animate-glow shadow-glow rounded-full"
+      >
+        <Image
+          src="/logo.svg"
+          width={60}
+          height={60}
+          alt="logo do projeto"
+          priority
+          unoptimized
+          className="w-20 h-20"
+        />
       </div>
+
       <ul className="w full flex justify-around gap-8">
         <ListItem text="Início" href="#inicio" />
         <ListItem text="Sobre mim" href="#about" />
