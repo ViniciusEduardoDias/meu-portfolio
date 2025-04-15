ff7718651840bd0ad02adfea1873873b467b1d50
Vinícius Dias <diasvinicius0401@gmail.com>
2025-04-15 11:08:47 -0300

    Primeira parte do refinamento dos estilos

--- src/components/Apresentation.tsx
+++ src/components/Apresentation.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaDownload } from "react-icons/fa";
 
 function Apresentation() {
   return (
@@ -18,7 +18,7 @@ function Apresentation() {
             href="https://www.linkedin.com/in/vinicius-ed-dias/"
             target="_blank"
             rel="noopener noreferrer"
-            className="w-12 h-12 flex items-center justify-center rounded-full bg-lime-300 text-black text-xl hover:scale-110 transition"
+            className="w-12 h-12 flex items-center justify-center rounded-full bg-lime-400 hover:bg-lime-200 text-black text-xl hover:scale-110 transition"
           >
             <FaGithub />
           </a>
@@ -26,20 +26,30 @@ function Apresentation() {
             href="https://www.linkedin.com/in/vinicius-ed-dias/"
             target="_blank"
             rel="noopener noreferrer"
-            className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-800 text-white text-xl hover:scale-110 transition"
+            className="w-12 h-12 flex items-center justify-center rounded-full text-white text-xl bg-blue-600 hover:bg-blue-800 hover:scale-110 transition"
           >
             <FaLinkedin />
           </a>
         </div>
       </div>
-      <div>
+      <div className="flex flex-col justify-center items-center mb-8">
         <Image
           src="/persondev.png"
           alt="Descrição da imagem"
           width={600}
-          height={600}
-          className="mb-24 sm:mb-0 rounded-md object-contain max-h-[80vh]"
+          height={200}
+          className="sm:mb-0 rounded-md object-contain max-h-[80vh]"
         />
+
+        <a
+          href="https://drive.google.com/file/d/190Q9NNsZz7-gNzoMya3dRjFwHv4gdQiF/view?usp=sharing"
+          target="_blank"
+          download
+          className="flex max-w-80 items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-800 transition"
+        >
+          <FaDownload />
+          Baixar Currículo
+        </a>
       </div>
     </section>
   );
